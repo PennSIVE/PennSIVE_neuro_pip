17eceaeeaa23e8a417d2447c856cf8dfebfe5133
elizabeth-horwath <eahorwath@gmail.com>
2026-05-14 15:21:51 -0400

    update hd-bet path

--- pipelines/alpaca/alpaca.sh
+++ pipelines/alpaca/alpaca.sh
@@ -24,7 +24,7 @@ show_help() {
   echo "  --sinpath   Specify the path to the singularity image if a singularity container is used. A default path is provided: /project/singularity_images/pennsive_amd64_cputorch.sif"
   echo "  --dockerpath   Specify the path to the docker image if a docker container is used. Note - running on MacOS, be sure the memory limit of the VM (set in Docker Desktop) is sufficiently large. A default path is provided: russellshinohara/pennsive_arm64_cputorch:v1.1"
   echo "  --dockermem   Specify the memory and swap allocated to the docker image if a docker container is used. A default is provided: 48g"
-  echo "  --hdbetpath   Specify the path to the HD-BET binary. For the Docker container, this is hard-coded in this version. If pre-downloading model, this should be in ~/hd-bet_params/release_2.0.0/. A default path is provided: /opt/fsl-6.0.7.19/bin/hd-bet"
+  echo "  --hdbetpath   Specify the path to the HD-BET binary. For the Docker container, this is hard-coded in this version. If pre-downloading model, this should be in ~/hd-bet_params/release_2.0.0/. A default path is provided: /opt/hd-bet-venv/bin/hd-bet"
   echo "  --toolpath   Specify the path to the saved pipeline folder, eg: /path/to/folder"
 }
 
@@ -55,7 +55,7 @@ c=cluster
 sin_path="/project/singularity_images/pennsive_amd64_cputorch.sif"
 tool_path=""
 docker_path=russellshinohara/pennsive_arm64_cputorch:v1.1
-hdbet_path="/opt/fsl-6.0.7.19/bin/hd-bet"
+hdbet_path="/opt/hd-bet-venv/bin/hd-bet"
 docker_mem=48g
 hdbet_model="~/hd-bet_params/"
 
@@ -245,7 +245,7 @@ if [ "$step" = "estimation" ]; then
             docker run --memory=$docker_mem --memory-swap=$docker_mem --rm -it -v $main_path:/home/main -v $tool_path:/home/tool $docker_path Rscript /home/tool/pipelines/alpaca/alpaca.R --mainpath /home/main \
             --participant $p --session $s --t1 $t1_r --flair $flair_r --epi_mag $epimag_r --epi_phase $epipha_r --n4 $n4 --skullstripping $skullstripping \
             --registration $registration --whitestripe $whitestripe --mimosa $mimosa --threshold $threshold \
-            --hdbetpath /opt/fsl-6.0.7.19/bin/hd-bet --lesioncenter /home/tool/lesioncenter --mpath /home/tool/pipelines/mimosa/model/mimosa_model.RData --helpfunc /home/tool/help_functions > $main_path/log/output/alpaca_output_${p}_${s}.log 2> $main_path/log/error/alpaca_error_${p}_${s}.log
+            --hdbetpath $hdbet_path --lesioncenter /home/tool/lesioncenter --mpath /home/tool/pipelines/mimosa/model/mimosa_model.RData --helpfunc /home/tool/help_functions > $main_path/log/output/alpaca_output_${p}_${s}.log 2> $main_path/log/error/alpaca_error_${p}_${s}.log
           fi
         done
     done
@@ -287,7 +287,7 @@ if [ "$step" = "estimation" ]; then
             docker run --memory=$docker_mem --memory-swap=$docker_mem --rm -it -v $main_path:/home/main -v $tool_path:/home/tool -v $HOME/hd-bet_params:/home/$USER/hd-bet_params -e HOME=/home/$USER $docker_path Rscript /home/tool/pipelines/alpaca/alpaca.R --mainpath /home/main \
             --participant $p --session $ses --t1 $t1_r --flair $flair_r --epi_mag $epimag_r --epi_phase $epipha_r --n4 $n4 --skullstripping $skullstripping \
             --registration $registration --whitestripe $whitestripe --mimosa $mimosa --threshold $threshold \
-            --hdbetpath /opt/fsl-6.0.7.19/bin/hd-bet --lesioncenter /home/tool/lesioncenter --mpath /home/tool/pipelines/mimosa/model/mimosa_model.RData --helpfunc /home/tool/help_functions > $main_path/log/output/alpaca_output_${p}_${ses}.log 2> $main_path/log/error/alpaca_error_${p}_${ses}.log
+            --hdbetpath $hdbet_path --lesioncenter /home/tool/lesioncenter --mpath /home/tool/pipelines/mimosa/model/mimosa_model.RData --helpfunc /home/tool/help_functions > $main_path/log/output/alpaca_output_${p}_${ses}.log 2> $main_path/log/error/alpaca_error_${p}_${ses}.log
       fi
   fi
 fi 
